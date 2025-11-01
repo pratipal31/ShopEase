5a6c0167d3ae49ab382baa38566cc1c5677b163e
jefin3273 <jefinjohn3273@gmail.com>
2025-11-01 13:13:10 +0530

    login and signup page

--- brained/src/App.tsx
+++ brained/src/App.tsx
@@ -1,14 +1,9 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LoginSignup from "./components/auth/LoginSignup.tsx"
 import './App.css'
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <LoginSignup />
   )
 }
 
